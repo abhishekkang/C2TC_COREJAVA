ff75b76d75af4aeb5640b7c55a3034a36f15d6a9
abhishekkang <89579588+abhishekkang@users.noreply.github.com>
2022-03-12 15:57:06 +0530

    update

--- day1/src/day1/Student.java
+++ day1/src/day1/Student.java
@@ -2,28 +2,41 @@ package day1;
 
 public class Student {
 	int no_of_students;
+	
 	String Name;
 	int grade;
+	
+	public int getNo_of_students() {
+		return no_of_students;
+	}
+
+	public void setNo_of_students(int no_of_students) {
+		this.no_of_students = no_of_students;
+	}
+
 	public void setGrade(int s)
 	{ this.grade=s;
 	
 	}
+	
 public int getGrade()
-{ return grade;
+{
+	return grade;
 }
 void display()
 		{
 	System.out.println("Welcome to the class " );
-	System.out.print(grade);
-	
-	
+	System.out.println(grade);
+	System.out.println(no_of_students);
 		}
 public static void main(String[] args)
 {
 	Student stu=new Student();
 	stu.display();
 	stu.setGrade(10);
+	stu.setNo_of_students(10);
 	System.out.println(stu.getGrade());
+	System.out.println(stu.getNo_of_students());
 	
 }
 }
--- day1/src/day1/helloword.java
+++ day1/src/day1/helloword.java
@@ -2,7 +2,7 @@ package day1;
 
 public class helloword {
 	public static void main (String[] args) {
-System.out.println("Hello worrld");
+System.out.println("Hello world");
 }
 }
 
