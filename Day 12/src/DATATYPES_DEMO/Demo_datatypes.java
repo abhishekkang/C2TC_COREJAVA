0cfe6654db965f07e8c9b381a10b99d16068dcfb
abhishekkang <89579588+abhishekkang@users.noreply.github.com>
2022-03-18 12:21:55 +0530

    Operators

---
+++ Day 12/src/DATATYPES_DEMO/Demo_datatypes.java
@@ -0,0 +1,17 @@
+package DATATYPES_DEMO;
+
+public class Demo_datatypes {
+
+	public static void main(String[] args) 
+	{
+
+		//char varl = '\u00A7';
+		//char varl = '\u00A5';
+		char varl = '\u00A9';
+		
+		System.out.println("The char value is "+varl);
+		
+	}
+
+}
+
