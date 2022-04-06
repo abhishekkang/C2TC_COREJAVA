4d7935e8ceff1e4bfd17fbdbc2e700ebb0cc2de4
abhishekkang <89579588+abhishekkang@users.noreply.github.com>
2022-04-06 16:05:33 +0530

    Multithread

---
+++ Day18/src/RunnableExample.java
@@ -0,0 +1,38 @@
+//https://github.com/abhishekkang/C2TC_COREJAVA/tree/master/Day18/src
+class Chrome implements Runnable {
+	public void run() {
+		System.out.println("Chrome id " + Thread.currentThread().getId());
+	}
+}
+
+class VSCode implements Runnable {
+	public void run() {
+		System.out.println("VSCode id " + Thread.currentThread().getId());
+	}
+
+}
+
+class Facebook implements Runnable {
+	public void run() {
+		System.out.println("Facebook id " + Thread.currentThread().getId());
+	}
+
+}
+
+public class RunnableExample {
+	public static void main(String[] args) {
+
+		Chrome ob = new Chrome();
+		Thread t = new Thread(ob);
+		t.start();
+
+		VSCode ob1 = new VSCode();
+		Thread t1 = new Thread(ob1);
+		t1.start();
+
+		Facebook ob2 = new Facebook();
+		Thread t2 = new Thread(ob2);
+		t2.start();
+	}
+
+}
