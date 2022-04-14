f941d7fe2e16c7afd1ed1b309fa47cdac2d32448
abhishekkang <89579588+abhishekkang@users.noreply.github.com>
2022-04-14 16:59:33 +0530

    HashSet,LinkedHashSet,TreeSet,HashMap

---
+++ Day22/src/HashMapDemo.java
@@ -0,0 +1,14 @@
+
+import java.util.HashMap;
+
+public class HashMapDemo {
+
+	public static void main(String[] args) {
+		HashMap<Integer, String> hmap = new HashMap<Integer, String>();
+		hmap.put(1, "Abhishek");
+		hmap.put(2, "Kshitiz");
+		System.out.println(hmap);
+
+	}
+
+}
